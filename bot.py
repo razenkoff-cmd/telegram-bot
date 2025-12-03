4c65e02025e1808eb0742966477c5d566ed3191b
razenkoff-cmd <razenkoff@mail.ru>
2025-12-03 23:54:45 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,11 @@
+import os
+import telebot
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Бот работает!")
+
+bot.polling()
